8e7a20f17bf37edd7cc0aae8153e7c4aef163b45
manasi-12-stack <67551633+manasi-12-stack@users.noreply.github.com>
2023-02-03 20:31:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,34 +16,35 @@ app.listen(port, () => {
 });
 
 cron.schedule("0 0 0 * * * *", async () => {
-  try {
-    const PK = config.CH_KEY;
-    const Pkey = `0x${PK}`;
-
-    const signer = new ethers.Wallet(Pkey);
-    let url =
-      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum-push-notification-service&vs_currencies=usd";
-    let response = await axios.get(url);
-
-    price = response.data.ethereum-push-notification-service.usd;
-
-    // apiResponse?.status === 204, if sent successfully!
-    const apiResponse = await PushAPI.payloads.sendNotification({
-      signer,
-      type: 1, // broadcast
-      identityType: 2, // direct payload
-      notification: {
-        title: `Today's PUSH price`,
-        body: `The price of PUSH is ${price} USD`,
-      },
-      payload: {
-        title: `Today's ETH price`,
-        body: `The price of eth is ${price} USD`,
-        cta: "",
-        img: "",
-      },
-      channel: `eip155:5:${config.CH_PKEY}`, // your channel address
-      env: "staging",
-    });
-  } catch (error) {}
-});
+    try {
+      const PK = config.CH_KEY;
+      const Pkey = `0x${PK}`;
+  
+      const signer = new ethers.Wallet(Pkey);
+      let url =
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum-push-notification-service&vs_currencies=usd";
+      let response = await axios.get(url);
+  
+      price = response.data.ethereum-push-notification-service.usd;
+  
+      // apiResponse?.status === 204, if sent successfully!
+      const apiResponse = await PushAPI.payloads.sendNotification({
+        signer,
+        type: 1, // broadcast
+        identityType: 2, // direct payload
+        notification: {
+          title: `Today's PUSH price`,
+          body: `The price of PUSH is ${price} USD`,
+        },
+        payload: {
+            title: `Today's PUSH price`,
+            body: `The price of PUSH is ${price} USD`,
+          cta: "",
+          img: "",
+        },
+        channel: `eip155:5:${config.CH_PKEY}`, // your channel address
+        env: "staging",
+      });
+      console.log(123);
+    } catch (error) {}
+  });
